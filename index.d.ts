64d015b83da8686447b16a658d71770af682c679
Luuk Boschker <luuk.boschker@gmail.com>
2021-11-19 10:40:41 +0100

    Get types right

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,15 @@
 declare module 'blueprinted-diagram-js';
+
+/**
+ * Create a new instance of a diagram editor.
+ * 
+ * @param options editor options
+ */
+declare function createEditor(options: EditorOptions): void;
+
+export interface EditorOptions {
+  /**
+   * The DOM element where the diagram is rendered.
+   */
+  container: Element;
+}
